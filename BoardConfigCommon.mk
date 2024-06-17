2eaf2989289d93c002ec219cc69c0a38091dc288
Michael Bestas <mkbestas@lineageos.org>
2024-06-17 10:41:47 +0300

    kitakami-common: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,7 +157,8 @@ TARGET_PROVIDES_LIBLIGHT := true
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-       $(COMMON_PATH)/device_framework_compatibility_matrix.xml
+    $(COMMON_PATH)/device_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml
 
 ifneq ($(BOARD_HAVE_RADIO),false)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_radio.xml
