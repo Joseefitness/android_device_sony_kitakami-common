0bee5f81b3c11ef0f1397a6d690ed7180ffc6ad8
Yongqin Liu <yongqin.liu@linaro.org>
2024-07-28 12:20:28 +0300

    kitakami-common: workaround: disable graphics driver preloading
    As the Mali userspace binaries were built for Android 8.0,
    when -ffixed-x18 was not guarteed to be used, which had been
    ensured for Android versions newer than P for the shadow
    call stack (SCS) feature.
    For such old version vendor blob, there was a workaround
    in com_android_internal_os_ZygoteInit.cpp which has been removed
    now.
        https://android-review.googlesource.com/c/platform/frameworks/base/+/2518435
    
    After that removal, there is a crash on libbluetooth_jni.so
    reported as the issue here:
        https://issuetracker.google.com/issues/302408537
    
    As a workaround, disabling the preloading of them to avoid
    the bluetooth crash issue.
    
    Bug: 302408537
    Test: bluetooth enabled by default after booted up
    
    Change-Id: Icd8be5e120b016b3019e0d0dd23121f7e931a67c
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    
    @acroreiser: the same but with Adreno binaries.

--- device-common.mk
+++ device-common.mk
@@ -367,4 +367,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
    macaddrsetup
 
+## This is a workaround for the Bluetooth sanitize shadow call stack (SCS)
+## crash reported here: https://issuetracker.google.com/issues/302408537.
+## For details of the root cause and the cts vts tests comparison between
+## the preloading and non-preloading builds, please check the above issue.
+PRODUCT_PROPERTY_OVERRIDES += ro.zygote.disable_gl_preload=1
+
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
